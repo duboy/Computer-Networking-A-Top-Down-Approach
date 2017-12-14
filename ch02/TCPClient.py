965b3a531b53cfe4c1d18d56e0b5a235377d38d9
duboy <futuredby@gmail.com>
2017-12-14 16:24:29 +0800

    change server to duboy.ddns.net

--- ch02/TCPClient.py
+++ ch02/TCPClient.py
@@ -1,5 +1,5 @@
 from socket import *
-serverName = '127.0.0.1'
+serverName = 'duboy.ddns.net'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
